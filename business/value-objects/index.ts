5050065718cf81829d5e704a264dfb9c1c38aced
Moisés Reis <reis.moreira.moises@gmail.com>
2026-08-30 00:22:38 -0300

    chore(business): add index barrels for business modules

---
+++ business/value-objects/index.ts
@@ -0,0 +1,6 @@
+export { default as GrowthFactor } from "./growth-factor.vo";
+export { default as PositiveMoney } from "./positive-money.vo";
+export { default as QuotaPrice } from "./quota-price.vo";
+export { default as QuotaQuantity } from "./quota-quantity.vo";
+export { default as SignedMoney } from "./signed-money.vo";
+export { default as SignedPercentage } from "./signed-percentage.vo";
